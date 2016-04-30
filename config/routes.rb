6109451af7ff52580065c249836a892322f265af
HustMaroon <thanhbk6294@gmail.com>
2016-04-30 15:17:55 +0700

    edit interface

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'static_pages#home'
   resources :sclasses
   resources :mini_works
   get '/rollup' => 'rollup#new'
